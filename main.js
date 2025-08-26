8d9e58403e49ae2e883876cee3cea4ec9a8d37ac
Adrian Anicete <anicete.ian14@gmail.com>
2025-08-26 17:18:33 +0800

    feat: add projects

--- main.js
+++ main.js
@@ -3,18 +3,59 @@ const aboutContainer = document.querySelector('.about-container');
 const homeBtn = document.querySelector('#home'); 
 const aboutBtn = document.querySelector('#about'); 
 const aboutBtn2 = document.querySelector('.about-btn');
+const month = document.querySelector('.month');
+const day = document.querySelector('.day');
+const year = document.querySelector('.year');
+const hour = document.querySelector('.hour');
+const min = document.querySelector('.sec');
+const pmAm = document.querySelector('.pm-am');
+
+// //get date
+let date = new Date();
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+const fullMonth = monthNames[date.getMonth()];
+const monthTrim = fullMonth.slice(0,3);
+
+month.textContent = monthTrim;
+day.textContent = date.getDate();
+year.textContent = date.getFullYear();
+
+//get time
+function updateDateTime() {
+  
+  const date = new Date(); 
+
+  let hours = date.getHours();
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12 || 12; 
+
+  let minutes = date.getMinutes();
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+
+  hour.textContent = hours;
+  min.textContent = minutes;
+  pmAm.textContent = ampm;
+}
+
+updateDateTime();
+
+setInterval(updateDateTime, 1000);
+
 
 function goToHome(event) {
-  event.preventDefault(); // Para sa links
-  event.stopPropagation(); // Para hindi mag-bubble
-  
+  event.preventDefault(); 
+  event.stopPropagation(); 
   heroContainer.style.display = 'flex';
   aboutContainer.style.display = 'none';
 }
 
 function goToAbout(event) {
-  event.preventDefault(); // Para sa links
-  event.stopPropagation(); // Para hindi mag-bubble
+  event.preventDefault(); 
+  event.stopPropagation(); 
   
   
   aboutContainer.style.display = 'flex';
